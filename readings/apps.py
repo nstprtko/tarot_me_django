c6cb8894becac6263896d34ec77471a7b322c7c9
Oleksandra Kasatkina <236292@student.uek.krakow.pl>
2025-11-21 21:14:39 +0100

    apps.py upd 21.11 21:14

--- readings/apps.py
+++ readings/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ReadingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'readings'
+
+    def ready(self):
+        # for signals.py - import signals so they are registered
+        import readings.signals
