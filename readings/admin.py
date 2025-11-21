a43f533c89ee9e18c37b18c203e797c8f5e43ad3
Oleksandra Kasatkina <236292@student.uek.krakow.pl>
2025-11-21 21:56:50 +0100

    admin.py 21.11 21:56

--- readings/admin.py
+++ readings/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Card, Profile, Reading
 
 # Register your models here.
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "is_premium", "daily_extra_uses", "last_reset")
+    readonly_fields = ("last_reset",)
+
+@admin.register(Reading)
+class ReadingAdmin(admin.ModelAdmin):
+    list_display = ("id", "reading_type", "user", "created_at", "extra_adds")
+    readonly_fields = ("cards", "ai_text", "created_at")
